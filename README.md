1186bfc5dbbeba39987978e5aeece500ef3efb0a
sahin-ismail <sahinismail0618@gmail.com>
2024-06-23 01:35:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# React JS Advanced Tutorial Notes
+Notes and codes I took while watching the React Js tutorial.
+
+![](header.png)
+
+## Installation and Run
+
+OS X & Linux:
+
+```sh
+npm install
+```
+```sh
+npm start
+```
---
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const UseStateBasics = () => {
+  const [text, setText] = useState("random title");
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </>
+  );
+};
+
+export default UseStateBasics;
